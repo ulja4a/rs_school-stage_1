dcea4c04d8c1542d27c1eb3379cc56247d9065f3
ulja4a <ulja4a@gmail.com>
2024-01-15 22:01:00 +0300

    feat: add connection between clickletter and letter in word and score and img hangman

--- hangman/index.js
+++ hangman/index.js
@@ -12,7 +12,7 @@ let score = document.createElement('span');
 comtainer.classList.add('wrapper');
 gallows.classList.add('gallows');
 gallowsImg.classList.add('gallows-img');
-gallowsImg.src = './images/gallows.svg';
+gallowsImg.src = './images/gallows-0.png';
 gallowsImg.alt = 'gallows picture';
 nameGame.classList.add('name-game');
 nameGame.textContent = 'Hangman Game';
@@ -33,66 +33,90 @@ gameBox.append(wordDisplay);
 //wordDisplay.append(letter);
 gameBox.append(questionDisplay);
 gameBox.append(guesses);
-guesses.append(score);
+
 
 // add keyboards buttons
 let keyboardDiv = document.createElement('div');
 keyboardDiv.classList.add('keyboard');
 gameBox.append(keyboardDiv);
 
+let currentGuesses = 0;
+let maxGuesses = 6;
+
+let initGame = (button, clickedLetter) => {
+  console.log(button, clickedLetter);
+  if(currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if(letter === clickedLetter) {
+        wordDisplay.querySelectorAll('li')[index].innerText = letter;
+        wordDisplay.querySelectorAll('li')[index].classList.add('active');
+      }
+    })
+  } else {
+    currentGuesses++;
+    gallowsImg.src = `./images/gallows-${currentGuesses}.png`
+  }
+  score.innerText = `${currentGuesses} / ${maxGuesses}`;
+}
+guesses.append(score);
+
 for (let i = 97; i <= 122; i++) {
   let button = document.createElement('button');
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
+  button.addEventListener('click', (e) => initGame(e.target, String.fromCharCode(i)));
 }
 
 // add word list
 let wordList = [
   {
-    word: "Nile",
+    word: "nile",
     question: "Longest river in the world"
   },
   {
-    word: "Hydrogen",
+    word: "hydrogen",
     question: "Element with the symbol 'H'"
   },
   {
-    word: " Paris",
+    word: " paris",
     question: "Capital of France"
   },
   {
-    word: "Armstrong",
+    word: "armstrong",
     question: "First man to step on the moon"
   },
   {
-    word: "Mars",
+    word: "mars",
     question: "Planet known as the Red Planet"
   },
   {
-    word: " Pacific",
+    word: "pacific",
     question: "Largest ocean on Earth"
   },
   {
-    word: "Einstein",
+    word: "einstein",
     question: "Famous physicist who formulated the theory of relativity"
   },
   {
-    word: "Skin",
+    word: "skin",
     question: "Human body's largest organ"
   },
   {
-    word: "Mercury",
+    word: "mercury",
     question: "Smallest planet in our solar system"
   },
   {
-    word: "Himalayas",
+    word: "himalayas",
     question: "Mountain range that includes Mount Everest"
   }
 ]
 
+let currentWord;
+
 // add random quation and word
 let getRandomWord = () => {
   let { word, question } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word;
   console.log(question);
   questionDisplay.innerText = question;
   wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
